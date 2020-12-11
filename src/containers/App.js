634a44a414c7cde427a0e91e2f5e5ec3af3559a0
Marcel van der Westhuizen <marcel.purplecry@gmail.com>
2020-12-11 15:01:22 +0200

    Title fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -15,8 +16,8 @@ class App extends Component {
 
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then( response => response.json() )
-            .then( users => this.setState({ robots: users}) );
+            .then( response => response.json())
+            .then( users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -29,6 +30,8 @@ class App extends Component {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
 
+        console.log(this.state.robots);
+
         if(!robots.length){
             return <h1>Loading</h1>
         }
@@ -37,7 +40,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
